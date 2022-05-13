279dfb7f4a83db96a00c434e8010241d591885ee
Alaasharaa <105313384+Alaasharaa@users.noreply.github.com>
2022-05-13 15:45:02 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const arrow = document.querySelector(".arrow");
+const movielist = document.querySelector(",movie-list");
+
+arrow.addEventListener("click" ,()=>{
+    movielist[i].style.transforms="translatex(-100px)"
+})
+
+
+
+
+console.log("arrow")
